da7fd8493fdedecd178597e30d62f7946c8b5993
marioenriqueb <marioenriquebaeza@gmail.com>
2019-08-19 21:06:38 +0100

    mejoras

--- src/main/java/com/exercise/converter/RenderingConverter.java
+++ src/main/java/com/exercise/converter/RenderingConverter.java
@@ -1,14 +1,16 @@
 package com.exercise.converter;
 
-import com.exercise.domain.LogRecord;
 import com.exercise.domain.Rendering;
-import com.exercise.utils.ReaderUtils;
 import com.exercise.domain.RenderingId;
 import com.exercise.domain.Report;
+import com.exercise.utils.ReaderUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class RenderingConverter {
@@ -17,42 +19,43 @@ public class RenderingConverter {
 
     public Report convert(List<String> lines) {
         Map<String, Rendering> renderings = new HashMap<>();
-        lines.forEach(line -> {
-            Optional<LogRecord> response = ReaderUtils.readLogLine(line);
-            if (response.isPresent()) {
-                LogRecord logRecord = response.get();
-                String message = logRecord.getMessage();
-                // Start Rendering
-                if (ReaderUtils.matchStartRenderingMessage(message)) {
-                    RenderingId renderingId = ReaderUtils.readStartRenderingMessage(message);
 
-                    if (renderings.get(renderingId) == null) {
-                        Rendering rendering = new Rendering();
-                        rendering.setId(renderingId);
-                        renderings.put(CURRENT, rendering);
-                    }
-                }
+        lines.stream()
+                .map(ReaderUtils::readLogLine)
+                .flatMap(optional -> optional.map(Stream::of).orElseGet(Stream::empty))
+                .forEach(logRecord -> {
+                    String message = logRecord.getMessage();
 
-                // UID Recived
-                if(ReaderUtils.matchRenderingUIDMessage(message)) {
-                    String uid = ReaderUtils.readRenderingUIDMessage(message);
-                    Rendering current = renderings.get(CURRENT);
-                    if (current != null){
-                        current.setUid(uid);
-                        current.addStart(logRecord.getTimestamp());
-                        renderings.put(uid, current);
-                    }
-                }
+                    // Start Rendering
+                    if (ReaderUtils.matchStartRenderingMessage(message)) {
+                        RenderingId renderingId = ReaderUtils.readStartRenderingMessage(message);
 
-                // Get Rendering
-                if(ReaderUtils.matchGetRenderingMessage(message)) {
-                    String uid = ReaderUtils.readGetRenderingMessage(message);
-                    if (renderings.get(uid) != null) {
-                        renderings.get(uid).addGet(logRecord.getTimestamp());
+                        if (renderings.get(renderingId) == null) {
+                            Rendering rendering = new Rendering();
+                            rendering.setId(renderingId);
+                            renderings.put(CURRENT, rendering);
+                        }
                     }
-                }
-            }
-        });
+
+                    // UID Recived
+                    if(ReaderUtils.matchRenderingUIDMessage(message)) {
+                        String uid = ReaderUtils.readRenderingUIDMessage(message);
+                        Rendering current = renderings.get(CURRENT);
+                        if (current != null){
+                            current.setUid(uid);
+                            current.addStart(logRecord.getTimestamp());
+                            renderings.put(uid, current);
+                        }
+                    }
+
+                    // Get Rendering
+                    if(ReaderUtils.matchGetRenderingMessage(message)) {
+                        String uid = ReaderUtils.readGetRenderingMessage(message);
+                        if (renderings.get(uid) != null) {
+                            renderings.get(uid).addGet(logRecord.getTimestamp());
+                        }
+                    }
+                });
 
         return generateReport(renderings);
     }
